b51271a85d493a19a4af247e02657268977b6909
Knut Ahlers <knut@ahlers.me>
2019-05-23 17:38:09 +0200

    Apply bootswatch theme
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import app from './app.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootswatch/dist/cosmo/bootstrap.min.css'
 import 'chartist/dist/chartist.min.css'
 
 Vue.use(BootstrapVue)
